e2e1597613bd628f3684fee4794473c09cf3bb09
Joseph-RY <github.joseph.x@gmail.com>
2025-02-07 21:59:01 +0500

    add files

---
+++ script.js
@@ -0,0 +1,151 @@
+// Task - 1
+
+// const Pi = 3.14;
+
+// let calculateCircleArea = function (x) {
+//     let result;
+//     return result = Pi * (x * x);
+// }
+
+// console.log(Math.ceil(calculateCircleArea(5)));
+// console.log(Math.ceil(calculateCircleArea(8)));
+// console.log(Math.floor(calculateCircleArea(3)));
+
+
+// Task - 2
+
+// let sumOfThreeDigit = function (x) {
+//     let numberOne =  x / 100;
+//     let numberTwo = x / 10 % 10;
+//     let numberThree = x % 10;
+//     return numberOne + numberTwo + numberThree;
+// }
+
+// console.log(Math.floor (sumOfThreeDigit(123)));
+// console.log(Math.floor (sumOfThreeDigit(345)));
+// console.log(Math.floor (sumOfThreeDigit(321)));
+
+
+// Task - 3
+
+// let sumOfDigits = function (x) {
+//     let result
+//     if (x%2==0) {
+//         return result = x + 1;
+//     }
+//     else return result = x - 2;
+// }
+
+// console.log(sumOfDigits(22));
+// console.log(sumOfDigits(4));
+// console.log(sumOfDigits(3));
+
+
+// Task - 4
+
+// let doubleEachDigit = function (x) {
+
+//     let result = '';
+
+//     for (let i = x; i > 0; i = Math.floor(i / 10))
+//     {
+//         let number = i % 10; 
+//         result = (number * 2) + result  ;
+//     }
+//     return result;
+// }
+
+// console.log(doubleEachDigit(7132));
+// console.log(doubleEachDigit(897));
+// console.log(doubleEachDigit(811));
+
+
+// Task - 5
+
+// let lessThan100 = function (a, b) {
+//     let result = '';
+
+//     if (a + b < 100) {
+//         return "true";
+//     }
+//     else 
+//     {
+//         return "false";
+//     }
+// }
+
+// console.log(lessThan100(22, 15));
+// console.log(lessThan100(83, 34));
+// console.log(lessThan100(3, 77));
+
+
+// Task - 6
+
+// let perimetr = function (x) {
+//     let result = '';
+
+//     return result = 4 * x;
+// }
+
+// console.log(perimetr(7));
+// console.log(perimetr(8));
+// console.log(perimetr(25));
+
+// Task - 7
+
+// let area = function (x) {
+//     let result = '';
+
+//     return result = x * x;
+// }
+
+// console.log(area(8));
+// console.log(area(11));
+// console.log(area(6));
+
+
+// Task - 8
+
+// let averageNum = function (a, b) {
+//     let result = '';
+
+//     return result = (a + b)/2;
+// }
+
+// console.log(averageNum(7, 9));
+// console.log(averageNum(8, 12));
+// console.log(averageNum(10, 30));
+
+
+
+// Task - 9
+
+// let evenOrOdd = function (x) {
+//     let result = '';
+
+//     if (x % 2 == 0){
+//         return result = 3;
+//     }
+//     else {
+//         return result = 4;
+//     }
+// }
+
+// console.log(evenOrOdd(7));
+// console.log(evenOrOdd(12));
+// console.log(evenOrOdd(-8));
+
+
+// Task - 10
+
+// let calculation = function (x) {
+//     let result = '';
+
+//     return result = 17*x * x - 6*x + 13;
+// }
+
+// console.log(calculation(2));
+// console.log(calculation(0));
+// console.log(calculation(10));
+
+
